46bc952618e182da99b3db38cd42661a516b1e6f
Kushwanth <kushwanth7@gmail.com>
2016-12-10 20:49:31 -0500

    SRILM

--- runKeywordModel.sh
+++ runKeywordModel.sh
@@ -4,6 +4,7 @@
 . path.sh
 
 nj=1
+lm_order=1
 
 #Prepare the Dictionary for the keyword model
 echo "========Preparing the dictionary for the keyword model============"
@@ -36,8 +37,32 @@ echo
 echo "==========Completed preparation of prepare lang ========"
 echo
 
+echo "=======Making lm.arpa======"
+echo
+local=data/local
+mkdir -p $local/tmp
 
+loc=`which ngram-count`;
+if [ -z $loc ]; then
+  if uname -a | grep 64 >/dev/null; then # some kind of 64 bit...
+    sdir=$KALDI_ROOT/tools/srilm/bin/i686-m64
+  else
+    sdir=$KALDI_ROOT/tools/srilm/bin/i686
+  fi
+  if [ -f $sdir/ngram-count ]; then
+    echo Using SRILM tools from $sdir
+    export PATH=$PATH:$sdir
+  else
+    echo You appear to not have SRILM tools installed, either on your path,
+    echo or installed in $sdir.  See tools/install_srilm.sh for installation
+    echo instructions.
+    exit 1
+  fi
+fi
 
+ngram-count -order $lm_order -write-vocab $local/tmp/vocab-full.txt -wbdiscount -text $local/corpus.txt -lm $local/tmp/lm.arpa
+
+echo "=======Completed making lm.arpa=========="
 
 
 
